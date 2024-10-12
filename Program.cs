0a42b046959095cf5c67b4a71add9e3cd28eeac7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-11 21:36:30 -0300

    solicite que o usuário digite sua altura e o seu gênero. Calcule e exiba seu peso ideal

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double altura, pesoIdeal;
+string genero;
+
+
+Console.WriteLine("Digite a sua altura em m..........:");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Genero [M]asculino / [F]eminino: ");
+genero = Console.ReadLine()!.ToUpper().Trim();
+if (genero != "M" && genero != "F")
+{ Console.WriteLine("Nao obtenho a formula para realizar o calculo");
+}
+else 
+{if (genero == "M")
+{ pesoIdeal = altura * 72.7 - 58.0;
+}
+else
+{ pesoIdeal = altura * 62.1 - 44.7;
+}
+
+Console.WriteLine($"O seu peso ideal é {pesoIdeal:N1}kg");
+}
